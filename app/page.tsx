3510d6f17fa417da605d2e0aed6c032570290112
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2025-02-14 11:26:48 -0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
+      <h2>'test</h2>
       <p className="mb-4">
         {`Hi, I'm Aaron, a software developer passionate about building intuitive and efficient applications. With experience in React, Flutter, and JavaScript, I focus on crafting responsive and user-centered interfaces. I enjoy problem-solving, learning new technologies, and bringing ideas to life through code.`}
       </p>
