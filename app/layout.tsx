12a6a0be254be884fe6ecc0f5b5488ff598461bc
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2025-03-17 14:42:40 -0500

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
@@ -47,12 +46,11 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
-        GeistSans.variable,
+        "text-black bg-white dark:text-white dark:bg-black font-iosevka",
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto font-iosevka">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
